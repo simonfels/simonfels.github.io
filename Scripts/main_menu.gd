1950617fa338418e2628a90092ef7631447162e1
Simon Fels <simon.fels@gmail.com>
2025-10-06 23:52:14 +0200

    mainmenu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+func _ready():
+	updateSoulLabel()
+
+func updateSoulLabel():
+	$Label2.text = str(SaveState.souls)
+
 func _on_start_button():
 	get_tree().change_scene_to_file("res://Scenes/level_one.tscn")
 
@@ -8,27 +14,37 @@ func _on_buy_articact1():
 		SaveState.souls -= 1
 		SaveState.artifact1 = true
 		$Artifact1.modulate = Color.WHITE
+		updateSoulLabel()
+		$Button5.queue_free()
 
 func _on_buy_articact2():
 	if SaveState.souls >= 2:
 		SaveState.souls -= 2
 		SaveState.artifact2 = true
 		$Artifact2.modulate = Color.WHITE
+		updateSoulLabel()
+		$Button4.queue_free()
 
 func _on_buy_articact3():
 	if SaveState.souls >= 3:
 		SaveState.souls -= 3
 		SaveState.artifact3 = true
 		$Artifact3.modulate = Color.WHITE
+		updateSoulLabel()
+		$Button3.queue_free()
 
 func _on_buy_articact4():
 	if SaveState.souls >= 4:
 		SaveState.souls -= 4
 		SaveState.artifact4 = true
 		$Artifact4.modulate = Color.WHITE
+		updateSoulLabel()
+		$Button2.queue_free()
 
 func _on_buy_articact5():
 	if SaveState.souls >= 5:
 		SaveState.souls -= 5
 		SaveState.artifact5 = true
 		$Artifact5.modulate = Color.WHITE
+		updateSoulLabel()
+		$Button.queue_free()
