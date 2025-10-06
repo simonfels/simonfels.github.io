54a64147dfb9023879f0b8b3d372fcaf891f8b1b
Simon Fels <simon.fels@gmail.com>
2025-10-07 01:43:23 +0200

    finishing

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -22,10 +22,12 @@ func updateSoulLabel():
 	$Label2.text = str(SaveState.souls)
 
 func _on_start_button():
+	$AudioStreamPlayer2D.play()
 	get_tree().change_scene_to_file("res://Scenes/level_one.tscn")
 
 func _on_buy_articact1():
 	if SaveState.souls >= 1:
+		$AudioStreamPlayer2D.play()
 		SaveState.souls -= 1
 		SaveState.artifact1 = true
 		$Artifact1.modulate = Color.WHITE
@@ -34,6 +36,7 @@ func _on_buy_articact1():
 
 func _on_buy_articact2():
 	if SaveState.souls >= 2:
+		$AudioStreamPlayer2D.play()
 		SaveState.souls -= 2
 		SaveState.artifact2 = true
 		$Artifact2.modulate = Color.WHITE
@@ -42,6 +45,7 @@ func _on_buy_articact2():
 
 func _on_buy_articact3():
 	if SaveState.souls >= 3:
+		$AudioStreamPlayer2D.play()
 		SaveState.souls -= 3
 		SaveState.artifact3 = true
 		$Artifact3.modulate = Color.WHITE
@@ -50,6 +54,7 @@ func _on_buy_articact3():
 
 func _on_buy_articact4():
 	if SaveState.souls >= 4:
+		$AudioStreamPlayer2D.play()
 		SaveState.souls -= 4
 		SaveState.artifact4 = true
 		$Artifact4.modulate = Color.WHITE
@@ -58,6 +63,7 @@ func _on_buy_articact4():
 
 func _on_buy_articact5():
 	if SaveState.souls >= 5:
+		$AudioStreamPlayer2D.play()
 		SaveState.souls -= 5
 		SaveState.artifact5 = true
 		$Artifact5.modulate = Color.WHITE
